d0497a248af2ef647ddcdc8c25b4773a9ac3a4e6
Smollny <smolllny@gmail.com>
2022-06-07 11:43:28 +0300

    Добавьте файлы проекта.

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,85 @@
+﻿// Сохраняем функцию 1  в переменную
+let contentFilter = function filterContent() {
+    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++) {
+        let videoText = elements[i].querySelector(".video-title").innerText;
+        if (!videoText.toLowerCase().includes(inputString.toLowerCase())) {
+            elements[i].style.display = 'none';
+        } else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
+// Обработка сессии (объявлено через expression)
+function handleSession() {
+    // Сохраним время начала сессии
+    session.set("startDate", new Date().toLocaleString())
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
+}
+// Сохраняем функцию 2 в переменную
+let sessionHandler = function handleSession() {
+    let session = new Map();
+    session.set("userAgent", window.navigator.userAgent)
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+
+    if (session.get("age") >= 18) {
+        let startDate = new Date().toLocaleString();
+
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+        session.set("startDate", startDate)
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+
+    for (let result of session) {
+        console.log(result)
+    }
+}
+
+function filterContent(userInput) { // Принимаем пользовательский ввод в качестве параметра.
+
+    // Находим контейнеры с видео, которые необходимо фильтровать
+    let elements = document.getElementsByClassName('video-container');
+
+    // Пробегаемся по контейнерам
+    for (let i = 0; i <= elements.length; i++) {
+        // Вытаскиваем текст описания видео, которое необходимо отфильтровать
+        let videoText = elements[i].querySelector(".video-title").innerText;
+
+
+        // Выполняем фильтрацию, сравнивая значения в нижнем регистре
+        if (!videoText.toLowerCase().includes(userInput.toLowerCase())) {
+            // Скрываем неподходящие
+            elements[i].style.display = 'none';
+        } else {
+            // Показываем подходящие
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
+
+function filterContent() {
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++) {
+        let videoText = elements[i].querySelector(".video-title").innerText;
+        if (!videoText.toLowerCase().includes(inputParseFunction() /* Захват переменной теперь происходит с помощью замыкания */.toLowerCase())) {
+            elements[i].style.display = 'none';
+        } else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
+
+// Логирование сессии (объявлено через function declaration)
+let sessionLog = function logSession(session) {
+    // Вывод в консоль
+    for (let result of session) {
+        console.log(result)
+    }
+}
